de18e92965d55fe28571fe0146a43ddf8a8fcb70
Caio <caiobarros1712@gmail.com>
2024-11-18 18:58:26 -0300

    created convit componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConviteComponent } from './convite/convite.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MaterialModule } from './Modules/material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConviteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
